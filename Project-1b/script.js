a47ab83ce6547d9549d5f83f78c00f92327e830c
syed asad <smasadullah300@gmail.com>
2021-06-12 18:51:01 +0500

    Initial commit

---
+++ Project-1b/script.js
@@ -0,0 +1,11 @@
+const form = document.getElementById('form');
+const username = document.getElementById('username');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const password2 = document.getElementById('password2');
+
+function showError(input, message) {
+    const formControl = input.parentElement;
+    formControl.className = 'form-control error';
+    const small = form
+}
